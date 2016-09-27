b447e214e8fae85cdf3997ef702d77bc5b63dd64
g9308370 <g9308370@hotmail.com>
2016-09-27 23:55:59 +0800

    init package

---
+++ src/JudgeServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Hchs\Judge;
+
+use Illuminate\Support\ServiceProvider;
+
+class JudgeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/migrations' => base_path('database/migrations'),
+        ], 'migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
